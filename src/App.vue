4c0ab531acbf1d6127932677fc7f5dd4958dd8dc
dainarakrewer <55606090+dainarakrewer@users.noreply.github.com>
2021-06-07 16:36:17 -0300

    07/06

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,33 @@
     <!-- parametro para o componente 
     <input type="text" v-model="nomeDainara">
     <Cliente :nome="nomeDainara" email="dainara@teste.com" idade="22"/>-->
-    <input type="text" v-model="clienteDainara.nome">
+    <!-- recebendo uma var de um array
+    <input type="text" v-model="clienteDainara.nome">-->
+    <!-- criando um campo de digitação para que dependendo do que foi digitado, aparece a div referente
+    IMPORTANTE: Está sem <hr> dps da primeira div pois o if n funciona com <hr>
+    <input type="text" v-model="texto">
     <hr>
-    <Cliente :cliente="clienteDainara"/>
-    <hr>
-    <Cliente :cliente="clienteJoao"/>                                                                                                                                                                                                                                                                                                                                          
-    <hr>
+    <div v-if="texto == 'Dainara'">
+      <Cliente :cliente="clienteDainara"/>
+    </div>
+    <div v-else-if="texto == 'João'">
+      <Cliente :cliente="clienteJoao"/> 
+    </div> 
+    <div v-else>
+      <Cliente :cliente="clienteDainara"/>
+      <hr>
+      <Cliente :cliente="clienteJoao"/> 
+    </div> -->
+    <div id="dainara">
+      <Cliente :cliente="clienteDainara"/>
+    </div><!--
+    --><div  id = "joao">
+      <Cliente :cliente="clienteJoao"/> 
+    </div>
     <!-- prop para preencher var manual
     <Cliente nome="Maria" email="maria@teste.com" idade="18"/>-->
     <hr>
-    <!--<Produto/>-->
+    <Produto />
   </div>
 </template>
 
@@ -26,11 +43,13 @@
 /*import HelloWorld from './components/HelloWorld.vue'*/
 //import: componente está sendo importado
 import Cliente from './components/Cliente'
-//import Produto from './components/Produto'
+import Produto from './components/Produto'
 export default {
   name: 'App',
   data(){
     return{
+      // var para parametro do if
+      //texto: "",
       //var para passar como parametro no componente
       //nomeDainara: "Dainara",
       clienteJoao: {
@@ -48,8 +67,8 @@ export default {
   },
   //componente foi setado
   components: {
-    Cliente
-    //Produto
+    Cliente,
+    Produto
   }
 }
 </script>
--- src/components/Produto.vue
+++ src/components/Produto.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <h3>Meu Produto </h3>
-        <p>Este é meu produto!</p>
+        <h2>Meus Produtos</h2>
+        <h3>Estes são meus produtos!</h3>
+        <div>
+            <!-- :key foi add por ter bug no for link:https://stackoverflow.com/questions/47608379/vue-language-server-elements-in-iteration-expect-to-have-v-bindkey-directiv-->
+            <p v-for="produto in Produtos" :key="produto.id">{{produto}}</p>
+        </div>
     </div>    
 </template>
 
 <script>
 //export default: pq quando vc cria um componente, é necessário exportar para outros arquivos
 export default  {
-    
+    data(){
+        return{
+            Produtos: ['Celular', 'TV', 'Fogão', 'Geladeira']
+        }
+    }
 }
 </script>
 
